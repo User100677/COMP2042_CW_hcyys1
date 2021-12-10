1445c5ef5ad698fb48a08a761898b35777e69eee
user100677 <dionsoo14384@gmail.com>
2021-12-10 18:19:46 +0800

    added new test case for ball class

--- src/test/java/Brick/Break/Ball/RubberBallControllerTest.java
+++ src/test/java/Brick/Break/Ball/RubberBallControllerTest.java
@@ -28,5 +28,78 @@ class RubberBallControllerTest{
         rubberBallController.moveTo(new Point(200, 300));
         assertNotNull(rubberBallController.getFaceBall());
     }
+    @Test
+    void getXBallUPTest(){
+        assertEquals(300, rubberBallController.getBallUp().getX());
+    }
+    @Test
+    void getYBallUPTest(){
+        assertEquals(425, rubberBallController.getBallUp().getY());
+    }
+    @Test
+    void getXBallDownTest(){
+        assertEquals(300, rubberBallController.getBallDown().getX());
+    }
+    @Test
+    void getYBallDownTest(){
+        assertEquals(435, rubberBallController.getBallDown().getY());
+    }@Test
+    void getXBallRightTest(){
+        assertEquals(305, rubberBallController.getBallRight().getX());
+    }
+    @Test
+    void getYBallRightTest(){
+        assertEquals(430, rubberBallController.getBallRight().getY());
+    }
+    @Test
+    void getXBallLeftTest(){
+        assertEquals(295, rubberBallController.getBallLeft().getX());
+    }
+    @Test
+    void getYBallLeftTest(){
+        assertEquals(430, rubberBallController.getBallLeft().getY());
+    }
+    @Test
+    void getXBallUpTest_AfterSetPointsMethod(){
+        rubberBallController.setPoints(300,425);
+        assertEquals(300, rubberBallController.getBallUp().getX());
+    }
+    @Test
+    void getYBallUpTest_AfterSetPointsMethod(){
+        rubberBallController.setPoints(300,425);
+        assertEquals(217.5, rubberBallController.getBallUp().getY());
+    }
+    @Test
+    void getXBallDownTest_AfterSetPointsMethod(){
+        rubberBallController.setPoints(300,425);
+        assertEquals(300, rubberBallController.getBallDown().getX());
+    }
+    @Test
+    void getYBallDownTest_AfterSetPointsMethod(){
+        rubberBallController.setPoints(300,425);
+        assertEquals(642.5, rubberBallController.getBallDown().getY());
+    }
+    @Test
+    void getXBallLeftTest_AfterSetPointsMethod(){
+        rubberBallController.setPoints(300,425);
+        assertEquals(150, rubberBallController.getBallLeft().getX());
+    }
+    @Test
+    void getYBallLeftTest_AfterSetPointsMethod(){
+        rubberBallController.setPoints(300,425);
+        assertEquals(430, rubberBallController.getBallLeft().getY());
+    }
+    @Test
+    void getXBallRightTest_AfterSetPointsMethod(){
+        rubberBallController.setPoints(300,425);
+        assertEquals(450, rubberBallController.getBallRight().getX());
+    }
+    @Test
+    void getYBallRightTest_AfterSetPointsMethod(){
+        rubberBallController.setPoints(300,425);
+        assertEquals(430, rubberBallController.getBallRight().getY());
+    }
+
+
 
 }
